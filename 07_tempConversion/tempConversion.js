b02fdbb64a76e7fb8da5e1ff937b7240560f266e
Laurens Vanderstraeten <laurensvanderstraeten@gmail.com>
2023-05-16 14:45:41 -0400

    07_tempConversion: implement the temp functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (degreesFahrenheit) {
+  return round((degreesFahrenheit - 32) * 5 / 9, 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (degreesCelsius) {
+  return round(degreesCelsius * 9 / 5 + 32, 1);
 };
+
+function round(value, precision) {
+  var multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
 
 // Do not edit below this line
 module.exports = {
