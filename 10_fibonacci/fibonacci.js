93deb42a288394ca8b51705bfc62570e80f23433
Laurens Vanderstraeten <laurensvanderstraeten@gmail.com>
2023-05-16 15:53:50 -0400

    10_fibonacci: implement fibonacci()

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+    if(number < 0) {
+        return 'OOPS';
+    }
+    fibonacciArray = [1, 1];
+    while (fibonacciArray.length < number) {
+        fibonacciArray.push(fibonacciArray[fibonacciArray.length - 2] + fibonacciArray[fibonacciArray.length - 1]);
+    }
+    return fibonacciArray[fibonacciArray.length - 1]
 };
 
 // Do not edit below this line
