fbc38783ac42e45fd58c44769f6f85a957de5807
Laurens Vanderstraeten <laurensvanderstraeten@gmail.com>
2023-05-16 11:35:08 -0400

    02_repeatString: implement repeatString()

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const errorString = 'ERROR';
 
+const repeatString = function (string, number) {
+    if(number < 0) {
+        return errorString;
+    }
+    let resultString = '';
+    for (let i = 0; i < number; i++) {
+        resultString = resultString.concat(string);
+    }
+    return resultString;
 };
 
 // Do not edit below this line
