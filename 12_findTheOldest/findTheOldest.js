9b7a6306448b796e2d715e021e785a543534edb2
Laurens Vanderstraeten <laurensvanderstraeten@gmail.com>
2023-05-16 16:19:42 -0400

    12_findTheOldest: implement findTheOldest()

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (persons) {
+    return persons.reduce(findTheOldestBetweenTwo);
 };
+
+function findTheOldestBetweenTwo(firstPerson, secondPerson) {
+    if (getAgeOfPerson(firstPerson) < getAgeOfPerson(secondPerson)) {
+        return secondPerson;
+    }
+    return firstPerson;
+}
+
+function getAgeOfPerson(person) {
+    let endDate = person.yearOfDeath;
+    if (!endDate) {
+        endDate = new Date().getFullYear();
+    }
+    return endDate - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
