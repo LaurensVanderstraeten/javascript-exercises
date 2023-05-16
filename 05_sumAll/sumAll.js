c0f245cdb37bd369091fcc6b3493db3f432bb494
Laurens Vanderstraeten <laurensvanderstraeten@gmail.com>
2023-05-16 13:21:57 -0400

    05_sumAll: implement sumAll()

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const ERRORSTRING = 'ERROR'
 
+const sumAll = function (firstNumber, secondNumber) {
+    if (!Number.isInteger(firstNumber) || !Number.isInteger(secondNumber)) {
+        return ERRORSTRING;
+    }
+    if (firstNumber < 0 || (secondNumber < 0)) {
+        return ERRORSTRING;
+    }
+    sumArray = new Array(firstNumber, secondNumber);
+    sumArray.sort();
+    let lowerBound = sumArray[0];
+    let upperBound = sumArray[1];
+    console.log(lowerBound);
+    for (let inBetweenNumber = ++lowerBound; inBetweenNumber < upperBound; inBetweenNumber++) {
+        sumArray.push(inBetweenNumber);
+    }
+    return sumArray.reduce((a, b) => a + b);
 };
 
 // Do not edit below this line
