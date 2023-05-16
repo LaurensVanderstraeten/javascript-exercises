ac9e1e9df35a7a11cd7c9a04142e978d43ffce32
Laurens Vanderstraeten <laurensvanderstraeten@gmail.com>
2023-05-16 15:38:49 -0400

    09_palindromes: implement palindromes()

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    parsedString = string.replace(/\W/g, '').toLowerCase();
+    reverseParsedString = reverseString(parsedString);
+    return parsedString === reverseParsedString;
+};
 
+function reverseString(string) {
+    return string.split('').reverse().join('');
 };
 
 // Do not edit below this line
